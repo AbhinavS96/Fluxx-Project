2bd750324c53abe0ae92b1b6165892b4bc679673
AbhinavS96 <abhinavsidharth@gmail.com>
2022-12-02 14:09:04 +0100

    added skeleton for Card

--- src/cards/Card.java
+++ src/cards/Card.java
@@ -1,5 +1,17 @@
 package cards;
 
-public class Card {
+import game.Game;
 
+public abstract class Card {
+	String cardName;
+	
+	public abstract void cardAction(Game game);
+	
+	public String getCardName() {
+		
+	}
+
+	public void setCardName(String cardName) {
+		
+	}
 }

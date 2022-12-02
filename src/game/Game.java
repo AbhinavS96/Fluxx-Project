0bcba415430e218fe41cb9ae972afdc81529e2f9
AbhinavS96 <abhinavsidharth@gmail.com>
2022-12-02 13:56:09 +0100

    added skeleton for game class

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,54 @@
 package game;
 
-public class Game {
+import java.util.List;
 
+import cards.Card;
+import cards.Goal;
+import player.Player;
+
+/**
+ * @author Abhinav
+ *
+ */
+
+public class Game {
+	
+	int playLimit, handLimit, drawLimit, keeperLimit;
+	Goal currentGoal;
+	List<Card> deck;
+	List<Card> discardPile;
+	List<Player> players;
+	Player currentPlayer;
+	
+	public void setPlayLimit(int playLimit) {
+		
+	}
+
+	public void setHandLimit(int handLimit) {
+
+	}
+
+	public void setDrawLimit(int drawLimit) {
+
+	}
+
+	public void setKeeperLimit(int keeperLimit) {
+	
+	}
+	
+	public void play() {
+		
+	}
+	
+	public void checkWinner() {
+		
+	}
+	
+	public void setDeck(List<Card> deck) {
+		
+	}
+	
+	public void viewRules() {
+		
+	}
 }

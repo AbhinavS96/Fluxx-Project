ed41a10b3ca0d3c464ab56c4e28461e16cc02e35
Abhinav <abhinavsidharth@gmail.com>
2022-12-23 23:34:38 +0100

    main loop completed

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,4 +1,7 @@
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import game.Game;
 
 public class GameManager {
@@ -14,9 +17,31 @@ public class GameManager {
 	}
 	
 	public static void main(String args[]) {
-		//check input for number of users -2
-		//user can also choose the game type -simplified fluxx
-		GameManager gameManager = new GameManager(2);
+		System.out.println("Welcome to Simplified Fluxx!!");
+		//There could also be an option to choose the type of Fluxx
+		int numberOfPlayers;
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			try {
+				System.out.println("Choose the number of players (2-6)");
+				numberOfPlayers = sc.nextInt();
+				if(numberOfPlayers < 2 || numberOfPlayers > 6) {
+					System.out.println("Please choose a number between 2 and 6...");
+					continue;
+				}
+				break;
+			}
+			//InputMismatchException reference: https://stackoverflow.com/questions/38830142/how-to-handle-invalid-input-when-using-scanner-nextint
+			catch (InputMismatchException e) {
+				System.out.println("Invalid input... Please try again.");
+			}
+			catch (Exception e) {
+				System.out.println("Something went wrong... Please try again.");
+			}
+		}
+		
+		GameManager gameManager = new GameManager(numberOfPlayers);
+		System.out.println("Starting the game!");
 		gameManager.startGame();
 	}
 
